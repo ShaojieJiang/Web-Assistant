ab5159f0a24b899395e7a3bacaa06275a7aaf53f
Shaojie Jiang <shaojiejiang.1991@gmail.com>
2023-10-22 13:59:56 +0200

    1 Implement using LangChain and change UI design to Streamlit (#2)
    * Reimplementing using LangChain.
    
    * Created a new UI in chat style.
    
    * Added streaming and feedback buttons.
    
    * Updated requirements.
    
    * Updated README.
    
    ---------
    
    Co-authored-by: Shaojie Jiang <shaojiejiang@MacBook.local>

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-# Open-WebGPT: An open version of WebGPT
+# Web Assistant: An open version of WebGPT
 
 Reproducing the WebGPT[^webgpt] work using `gpt-3.5-turbo` api calls and DuckDuckGo search.
-Enables flagging for data annotation.
+Enables votting for data annotation.
+Implemented using LangChain, Streamlit (for deployment), and Gradio (for fast prototyping).
 
 [^webgpt]: [Nakano, Reiichiro, et al. "Webgpt: Browser-assisted question-answering with human feedback." arXiv preprint arXiv:2112.09332 (2021).](https://arxiv.org/abs/2112.09332)
 
@@ -10,7 +11,7 @@ Example usages:
 * Annotate comparisons data using flagging for RM training, which is essential for RLHF
 * Used as a prototype for designing your RM data collection interface
 
-The annotated data will be written to your local path under `flagged`
+<!-- The annotated data will be written to your local path under `flagged` -->
 
 ## Setup
 
@@ -22,17 +23,22 @@ export OPENAI_API_KEY=you_openai_api_key
 
 ## Start
 
-Simply run this in your terminal `python open_webgpt.py`
+Simply run this in your terminal:
+```shell
+streamlit run web_assistant.py
+```
 
-## Sharing with your friends or family
+<!-- ## Sharing with your friends or family
 
 If you want to share the tool running on your machine, you can change `share: false` to `share: true` in `config.yaml`.
 Then after restarting the program, you will see a temporary link hosted by Gradio that can be accessed by others.
-> _NOTE:_ Sharing this link will allow others to use the tool that costs your OpenAI credits, please be aware of that.
+> _NOTE:_ Sharing this link will allow others to use the tool that costs your OpenAI credits, please be aware of that. -->
 
 ## TODO's
 
-- [ ] Query rewriting in-between the latest message and response
+- [ ] Query rewriting in-between the latest message and response (making use of chat history)
+- [ ] Simplify chain calls and prompts
 - [ ] Add references
-- [ ] Deployment of open-source models
+- [ ] Add crawled webpages for debugging
 - [ ] Summarise webpages using models/OpenAI api
+- [ ] Deployment of open-source models
---
+++ web_assistant.py
@@ -0,0 +1,122 @@
+from typing import Any
+
+import streamlit as st
+from langchain.callbacks.base import BaseCallbackHandler
+from langchain.chains import RetrievalQAWithSourcesChain
+from langchain.utilities.duckduckgo_search import DuckDuckGoSearchAPIWrapper
+from streamlit_feedback import streamlit_feedback
+
+from retrievers.web_research import WebRetriever as WebResearchRetriever
+
+
+class StreamHandler(BaseCallbackHandler):
+    def __init__(self, container=None, initial_text=""):
+        self.text = initial_text
+        self.container = container
+
+    def on_llm_new_token(self, token: str, **kwargs) -> None:
+        self.text += token
+        # TODO: Some tokens may not appear in the final response
+        with self.container:
+            message = st.chat_message("assistant")
+            message.write(self.text)
+
+    def on_chain_start(
+        self,
+        *args,
+        **kwargs,
+    ) -> Any:
+        with self.container:
+            st.status("Running...")
+
+
+def initialize_page():
+    title = "Web Assistant"
+    st.set_page_config(page_title=title, page_icon="🌐")
+    st.session_state.setdefault("user", [])
+    st.session_state.setdefault("assistant", [])
+    st.sidebar.title(title)
+
+
+def setup():
+    # Vectorstore
+    import faiss
+    from langchain.docstore import InMemoryDocstore
+    from langchain.embeddings.openai import OpenAIEmbeddings
+    from langchain.vectorstores import FAISS
+
+    embeddings_model = OpenAIEmbeddings()
+    embedding_size = 1536
+    index = faiss.IndexFlatL2(embedding_size)
+    vectorstore_public = FAISS(
+        embeddings_model.embed_query, index, InMemoryDocstore({}), {}
+    )
+
+    # LLM
+    from langchain.chat_models import ChatOpenAI
+
+    llm = ChatOpenAI(model_name="gpt-3.5-turbo-16k", temperature=0, streaming=True)
+
+    # Search
+    search = DuckDuckGoSearchAPIWrapper()
+
+    # Initialize
+    web_retriever = WebResearchRetriever.from_llm(
+        vectorstore=vectorstore_public, llm=llm, search=search, num_search_results=2
+    )
+
+    return web_retriever, llm
+
+
+def on_btn_click():
+    del st.session_state.user[:]
+    del st.session_state.assistant[:]
+
+
+def record_feedback(*args, **kwargs):
+    # TODO: Implement feedback logging
+    vote = args[0]
+    print(f"Response {kwargs['ind']} has the vote of {vote['score']}")
+
+
+# Main app page
+initialize_page()
+web_retriever, llm = setup()  # Init retriever and llm
+qa_chain = RetrievalQAWithSourcesChain.from_chain_type(llm, retriever=web_retriever)
+
+# st.chat_input(on_submit=refresh_page, key="user_input")
+user_input = st.chat_input(key="user_input")
+st.sidebar.button("Clear message", on_click=on_btn_click)
+if user_input:
+    st.session_state.user.append(user_input)
+    st.session_state.assistant.append(
+        {"type": "normal", "data": ""}
+    )  # Create a placeholder response in the session_state
+
+for i in range(len(st.session_state.assistant)):
+    st.chat_message("user").write(st.session_state.user[i])
+    assistant_msg = st.session_state.assistant[i]["data"]
+    if assistant_msg:
+        st.chat_message("assistant").write(st.session_state.assistant[i]["data"])
+    else:  # Calls to the chain is only triggered when a placeholder is detected
+        container = st.empty()
+        with container:
+            st.chat_message("assistant").write("")
+            stream_handler = StreamHandler(container)
+            response = qa_chain(
+                {"question": user_input}, callbacks=[stream_handler]
+            )  # Update the empty container in the callback
+            st.session_state.assistant[-1] = {  # Update the placeholder response
+                "type": "normal",
+                "data": response["answer"],
+            }
+            st.chat_message("assistant").write(st.session_state.assistant[-1]["data"])
+
+    feedback = streamlit_feedback(
+        feedback_type="thumbs",
+        key=f"feedback_{i}",
+        on_submit=record_feedback,
+        kwargs={"ind": i},
+    )  # triggers re-rendering after clicking
+
+# TODO: Log conversations
